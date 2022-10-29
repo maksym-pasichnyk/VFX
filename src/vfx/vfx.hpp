2ed9fc3ff926ca33f9d2b2d000376267b20b9b1b
Maksym Pasichnyk <maximmkaaa@gmail.com>
2022-10-29 22:56:02 +0300

    removed unnecessary dependencies

---
+++ src/vfx/vfx.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "delegate.hpp"
+#include "context.hpp"
+#include "device.hpp"
+#include "buffer.hpp"
+#include "group.hpp"
+#include "layer.hpp"
+#include "pass.hpp"
+#include "types.hpp"
+#include "queue.hpp"
+#include "assets.hpp"
+#include "signal.hpp"
+#include "texture.hpp"
+#include "material.hpp"
